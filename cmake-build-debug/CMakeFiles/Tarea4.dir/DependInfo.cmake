b853ff7c8925629e71df42c4f08d7517f3e54091
Aragorn2209 <ramonbust2209@gmail.com>
2017-08-30 18:09:52 -0600

    Tarea4
    Tarea4_Zebra

---
+++ cmake-build-debug/CMakeFiles/Tarea4.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/rbara/CLionProjects/Tarea4/Evaluador.cpp" "C:/Users/rbara/CLionProjects/Tarea4/cmake-build-debug/CMakeFiles/Tarea4.dir/Evaluador.cpp.obj"
+  "C:/Users/rbara/CLionProjects/Tarea4/Mascota.cpp" "C:/Users/rbara/CLionProjects/Tarea4/cmake-build-debug/CMakeFiles/Tarea4.dir/Mascota.cpp.obj"
+  "C:/Users/rbara/CLionProjects/Tarea4/main.cpp" "C:/Users/rbara/CLionProjects/Tarea4/cmake-build-debug/CMakeFiles/Tarea4.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Tarea4.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea4.dir/main.cpp.obj"
+  "CMakeFiles/Tarea4.dir/Evaluador.cpp.obj"
+  "CMakeFiles/Tarea4.dir/Mascota.cpp.obj"
+  "Tarea4.pdb"
+  "Tarea4.exe"
+  "Tarea4.exe.manifest"
+  "libTarea4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
